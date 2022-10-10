012039c2771c8f427708f2e1bd0c533934d304c8
Revarestu23 <nugraharestu75@gmail.com>
2022-10-10 22:33:55 +0700

    menambahkan membuat templating website sederhana

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Ramadhan Andreyanto
+            Saya tinggal di daerah Legok
+            olah raga yang saya sukai adalah
+            Bulutangkis";
+    }
+}
